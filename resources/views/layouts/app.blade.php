e7a2b14e796a98923f951b1b650f6f1c4794b344
Joel <hola@joelcuevas.com>
2024-12-15 23:02:54 -0600

    preload fontawesome to prevent glitches

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        
+        <link rel="preload" href="https://kit.fontawesome.com/3c033f8319.js" as="script" crossorigin="anonymous">
         <script src="https://kit.fontawesome.com/3c033f8319.js" crossorigin="anonymous"></script>
 
         <!-- Scripts -->
