626ae74862ea265e5afa754ff2ab7424d5f9487a
Joel <hola@joelcuevas.com>
2024-12-17 22:47:56 -0600

    added horizon snapshot command

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('chascity:pending')->everyMinute();
 Schedule::command('url:scheduled')->everyMinute();
+
+Schedule::command('horizon:snapshot')->everyFiveMinutes();
 Schedule::command('telescope:prune')->daily();
