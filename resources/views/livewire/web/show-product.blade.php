47e0423c0634a2d4f0a2df4385c5a485d39245ce
Joel <hola@joelcuevas.com>
2024-12-19 17:53:25 -0600

    fixed duplicated brands

--- resources/views/livewire/web/show-product.blade.php
+++ resources/views/livewire/web/show-product.blade.php
@@ -4,11 +4,11 @@
             <div class="flex items-start justify-between space-x-5">
                 <div>
                     <div class="text-sm sm:flex sm:items-center truncate w-full">
-                        <a href="{{ $product->store_link }}" class="block sm:inline  text-gray-600 hover:text-gray-900">{{ $product->store->name }} {{ $product->store->name }} {{ $product->store->name }}</a>
+                        <a href="{{ $product->store_link }}" class="block sm:inline  text-gray-600 hover:text-gray-900">{{ $product->store->name }}</a>
                         <span class="text-gray-300 px-2">/</span>
                         <a href="{{ $product->category_link }}" class="block sm:inline  text-gray-600 hover:text-gray-900">{{ $product->category }}</a>
                         <span class="text-gray-300 px-2">/</span>
-                        <a href="{{ $product->category_brand_link }}" class="block sm:inline  text-gray-600 hover:text-gray-900">{{ $product->brand }} {{ $product->brand }} {{ $product->brand }} {{ $product->brand }}</a>
+                        <a href="{{ $product->category_brand_link }}" class="block sm:inline  text-gray-600 hover:text-gray-900">{{ $product->brand }}</a>
                     </div>
                     <h1 class="mt-2 text-2xl font-semibold tracking-tight text-balance text-gray-900 sm:text-4xl">{{ $product->title }}</h1>
                     <div class="mt-2 text-gray-600 text-sm">SKU: {{ $product->sku }}</div>
