18919de9ab0a992a88011626e5e6e54dd0233ebc
Joel <hola@joelcuevas.com>
2024-12-19 23:38:36 -0600

    add ads config

---
+++ config/ads.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+    'enabled' => env('ADS_ENABLED', false),
+
+];
--- resources/views/components/product-grid.blade.php
+++ resources/views/components/product-grid.blade.php
@@ -1,16 +1,19 @@
 <div>
-            <!-- product-grid -->
-            <ins class="adsbygoogle"
-                style="display:block"
-                data-ad-client="ca-pub-9285674270424452"
-                data-ad-slot="3526061510"
-                data-ad-format="auto"
-                data-full-width-responsive="true"></ins>
-            <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
-        @foreach ($products->chunk(12) as $group)
-            <div class="grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-4 lg:grid-cols-6 lg:gap-x-8">
+    @if (config('ads.enabled'))
+        <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-9285674270424452"
+            data-ad-slot="3526061510"
+            data-ad-format="auto"
+            data-full-width-responsive="true">
+        </ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    @endif
+
+    @foreach ($products->chunk(12) as $group)
+        <div class="grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-4 lg:grid-cols-6 lg:gap-x-8">
             @foreach ($group as $product)
                 <a href="{{ $product->link }}" title="{{ $product->title }}" class="group text-sm w-full">
                     <div class="bg-gray-100/80 rounded-xl p-2 object-center w-full aspect-[3/4] group-hover:bg-gray-200/70">
@@ -21,8 +24,9 @@
                     <p class="mt-1 font-medium text-gray-900"><x-price :$product /></p>
                 </a>
             @endforeach
-            </div>
-        @endforeach
+        </div>
+    @endforeach
+    
     @if ($products instanceof \Illuminate\Pagination\AbstractPaginator)
         <div class="mt-12">{{ $products->links() }}</div>
     @endif
