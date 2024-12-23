3f533b58bcddde0e925dbb3ae2835735cdbb0611
Joel <hola@joelcuevas.com>
2024-12-23 14:14:40 -0600

    pause chascity queueing

--- routes/console.php
+++ routes/console.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('chascity:pending')->everyMinute();
-Schedule::command('url:scheduled --domain=preciominimo.chascity.com --limit=25')->everyMinute();
+// Schedule::command('chascity:pending')->everyMinute();
+// Schedule::command('url:scheduled --domain=preciominimo.chascity.com --limit=25')->everyMinute();
 
 Schedule::command('url:scheduled --limit=50')->everyMinute();
 
