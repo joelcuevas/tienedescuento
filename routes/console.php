a913754f17ab8f5c17fd5d7070d3a06811fc84bf
Joel <hola@joelcuevas.com>
2024-12-24 11:33:25 -0600

    schedule costco independently

--- routes/console.php
+++ routes/console.php
@@ -6,7 +6,8 @@ Schedule::command('chascity:pending --limit=15')->everyMinute();
 Schedule::command('liverpool:pending --limit=50')->everyMinute();
 
 Schedule::command('url:scheduled --domain=preciominimo.chascity.com --limit=15')->everyMinute();
-Schedule::command('url:scheduled --domain=www.liverpool.com.mx --domain=www.costco.com.mx --limit=100')->everyMinute();
+Schedule::command('url:scheduled --domain=www.liverpool.com.mx --limit=50')->everyMinute();
+Schedule::command('url:scheduled --domain=www.costco.com.mx --limit=50')->everyMinute();
 
 Schedule::command('horizon:snapshot')->everyFiveMinutes();
 Schedule::command('telescope:prune')->daily();
