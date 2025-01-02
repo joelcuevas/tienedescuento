e2f19c39fa6a16b4b5ccf9ed1973a7fa89c03693
Joel <hola@joelcuevas.com>
2025-01-02 12:04:35 -0600

    re-enable chascity

--- routes/console.php
+++ routes/console.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('fix:urls')->everyMinute();
 
-//Schedule::command('chascity:pending --limit=15')->everyMinute();
+Schedule::command('chascity:pending --limit=15')->everyMinute();
 //Schedule::command('liverpool:pending --limit=50')->everyMinute();
 
-//Schedule::command('url:scheduled --domain=preciominimo.chascity.com --limit=15')->everyMinute();
+Schedule::command('url:scheduled --domain=preciominimo.chascity.com --limit=15')->everyMinute();
 //Schedule::command('url:scheduled --domain=www.liverpool.com.mx --limit=50')->everyMinute();
 Schedule::command('url:scheduled --domain=www.costco.com.mx --limit=50')->everyMinute();
 
