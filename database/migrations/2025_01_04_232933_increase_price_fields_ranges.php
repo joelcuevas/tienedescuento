1342fce7432614e800656b97e5938aac0b8118fa
Joel <hola@joelcuevas.com>
2025-01-05 00:24:16 -0600

    make the prices nullable

--- database/migrations/2025_01_04_232933_increase_price_fields_ranges.php
+++ database/migrations/2025_01_04_232933_increase_price_fields_ranges.php
@@ -12,12 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->decimal('latest_price', 12, 2)->change();
-            $table->decimal('minimum_price', 12, 2)->change();
-            $table->decimal('maximum_price', 12, 2)->change();
-            $table->decimal('regular_price', 12, 2)->change();
-            $table->decimal('regular_price_upper', 12, 2)->change();
-            $table->decimal('regular_price_lower', 12, 2)->change();
+            $table->decimal('latest_price', 12, 2)->nullable()->change();
+            $table->decimal('minimum_price', 12, 2)->nullable()->change();
+            $table->decimal('maximum_price', 12, 2)->nullable()->change();
+            $table->decimal('regular_price', 12, 2)->nullable()->change();
+            $table->decimal('regular_price_upper', 12, 2)->nullable()->change();
+            $table->decimal('regular_price_lower', 12, 2)->nullable()->change();
         });
 
         Schema::table('prices', function (Blueprint $table) {
