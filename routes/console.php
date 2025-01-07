5c0d025aee02d13e9d25e997b1cfb0ba93e33d7f
Joel <hola@joelcuevas.com>
2025-01-07 15:20:59 -0600

    resume chascity

--- routes/console.php
+++ routes/console.php
@@ -7,7 +7,7 @@ Schedule::command('fix:urls')->everyMinute();
 Schedule::command('chascity:discover --limit=15')->everyMinute();
 Schedule::command('liverpool:discover --limit=25')->everyMinute();
 
-Schedule::command('url:scheduled --domain=preciominimo.chascity.com --limit=0')->everyMinute();
+Schedule::command('url:scheduled --domain=preciominimo.chascity.com --limit=25')->everyMinute();
 Schedule::command('url:scheduled --domain=www.liverpool.com.mx --limit=25')->everyMinute();
 Schedule::command('url:scheduled --domain=www.elpalaciodehierro.com --limit=25')->everyMinute();
 Schedule::command('url:scheduled --domain=www.costco.com.mx --limit=50')->everyMinute();
