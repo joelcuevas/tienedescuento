4e4c2c9c4802bf3aabcc9299f2de971954a733de
Joel <hola@joelcuevas.com>
2025-01-09 22:31:16 -0600

    adjusted aspect of store logos

--- resources/views/livewire/web/show-home.blade.php
+++ resources/views/livewire/web/show-home.blade.php
@@ -7,33 +7,17 @@
     <meta name="keywords" content="descuentos, ofertas, rastreador de precios, price tracker, promociones, precios bajos, seguimiento de precios, mejor precio, {{ country() }}, {{ mmyy() }}, {{ $stores->pluck('name')->unique()->join(', ') }}">
 </x-slot>
 
-<div class="space-y-10">
-    <h1 class="font-medium text-gray-900">Los mejores descuentos de {{ country() }} en {{ mmyy() }}</h1>
-
-    <ul class="flex space-x-6">
-        @foreach ($stores as $store)
-            <li>
-                <a href="{{ $store->link() }}" alt="{{ $store->name }}" class="flex flex-col justify-center items-center space-y-2">
-                    <img class="h-24 w-24 rounded-full p-0.5 border border-gray-200 hover:border-gray-400/50" src="{{ $store->image_url }}" alt="Ver descuentos de {{ $store->name }}">
+<div class="space-y-12">
+    <div class="mb-12">
+        <div class="grid grid-cols-4 gap-x-4 lg:gap-x-8 gap-y-4 lg:gap-y-12 sm:grid-cols-6 lg:grid-cols-10">
+            @foreach ($stores as $store)
+                <a href="{{ $store->link() }}" alt="{{ $store->name }}" class="w-20 sm:w-24 aspect-square flex flex-col justify-center items-center space-y-2">
+                    <img class="rounded-full p-0.5 border border-gray-200 hover:border-gray-400/50" src="{{ $store->image_url }}" alt="Ver descuentos de {{ $store->name }}">
                     <div class="text-xs text-gray-600">{{ $store->name }}</div>
                 </a>
-            </li>
-        @endforeach
-    </ul>
-
-    @if (config('ads.enabled'))
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9285674270424452" crossorigin="anonymous"></script>
-        <ins class="adsbygoogle"
-            style="display:block"
-            data-ad-client="ca-pub-9285674270424452"
-            data-ad-slot="3526061510"
-            data-ad-format="auto"
-            data-full-width-responsive="true">
-        </ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-    @endif
+            @endforeach
+        </div>
+    </div>
 
     <div class="space-y-12">
         @foreach ($taxonomies as $taxonomy)
