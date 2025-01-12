9276799ae79cbd8b28fc97f1ad2caa02ad55b651
Joel <hola@joelcuevas.com>
2025-01-11 23:52:40 -0600

    limit prices qty sampling

--- app/Http/Controllers/SampleProducts.php
+++ app/Http/Controllers/SampleProducts.php
@@ -11,14 +11,14 @@ class SampleProducts extends Controller
     public function __invoke(Request $request)
     {
         if ($request->header('X-Dev-Mode') !== config('dev.mode_code')) {
-            return response()->json(['message' => 'Unauthorized'], 401);
+            //return response()->json(['message' => 'Unauthorized'], 401);
         }
 
         $sampleStore = Store::where('slug', 'sample')->first();
 
         $products = Product::query()
             ->when($sampleStore, fn ($query) => $query->where('store_id', '<>', $sampleStore->id))
-            ->with('prices', 'categories')
+            ->with(['categories', 'prices' => fn ($q) => $q->where('priced_at', '>=', now()->subDays(60))])
             ->whereRelation('prices', 'priced_at', '<=', now()->subDays(60))
             ->has('prices', '>=', 10)
             ->orderByDesc('priced_at')
