902b7523e1acd78d057e3370b0adb4d462e33074
Joel <hola@joelcuevas.com>
2025-01-20 18:43:12 -0600

    added reset option to taxonomy builder

--- app/Console/Commands/TaxonomyBuild.php
+++ app/Console/Commands/TaxonomyBuild.php
@@ -7,16 +7,24 @@ use App\Models\Taxonomy;
 use App\Support\TaxonomyBuilder;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
 
 class TaxonomyBuild extends Command
 {
-    protected $signature = 'taxonomy:build';
+    protected $signature = 'taxonomy:build {--reset}';
 
     protected $description = 'Build a hierarchical taxonomy tree for categories';
 
     public function handle()
     {
+        if ($this->option('reset')) {
+            Schema::disableForeignKeyConstraints();
+            DB::table('category_taxonomy')->truncate();
+            DB::table('taxonomies')->truncate();
+            Schema::enableForeignKeyConstraints();
+        }
+
         new TaxonomyBuilder();
     }
 }
