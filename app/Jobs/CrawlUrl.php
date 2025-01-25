9a88abc67f12ee935aa6bfff2ee9f6cdd9e342dd
Joel <hola@joelcuevas.com>
2025-01-24 23:16:53 -0600

    make crawl url jobs unique

--- app/Jobs/CrawlUrl.php
+++ app/Jobs/CrawlUrl.php
@@ -3,16 +3,22 @@
 namespace App\Jobs;
 
 use App\Models\Url;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 
-class CrawlUrl implements ShouldQueue
+class CrawlUrl implements ShouldQueue, ShouldBeUnique
 {
     use Queueable;
 
     public function __construct(
         protected Url $url,
     ) {}
+
+    public function uniqueId(): string
+    {
+        return $this->url->id;
+    }
 
     public function handle(): void
     {
